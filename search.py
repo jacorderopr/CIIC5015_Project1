1aa80e8314e4577fb8dd8aeb6c3fe23c20a61ec3
Jose Alejandro <jacorderopr@gmail.com>
2024-03-03 19:38:15 -0400

    initial commit for implementUcs branch

--- search.py
+++ search.py
@@ -136,6 +136,7 @@ def breadthFirstSearch(problem: SearchProblem):
 def uniformCostSearch(problem: SearchProblem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
+    # Dijkstra's algorithm
     util.raiseNotDefined()
 
 def nullHeuristic(state, problem=None):
